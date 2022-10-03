55391794f289ffe4b3eb5ae51f7add0b210c82b1
Jos van Bakel <jos@codeaddict.org>
2022-10-03 16:17:22 +0200

    Update mautrix-discord

--- pkgs/mautrix-discord/default.nix
+++ pkgs/mautrix-discord/default.nix
@@ -1,27 +1,27 @@
-{ lib, buildGoModule, fetchFromGitLab, olm }:
+{ lib, buildGoModule, fetchFromGitHub, olm }:
 
 buildGoModule rec {
   pname = "mautrix-discord";
-  version = "unstable-2022-04-22";
+  version = "unstable-2022-09-19";
 
-  src = fetchFromGitLab {
-    owner = "beeper";
+  src = fetchFromGitHub {
+    owner = "mautrix";
     repo = "discord";
-    rev = "145c0cc2cbcfeabd13dfbfb8f9b844d2d37a6dcc";
-    sha256 = "sha256-FvPYUj6x47JSgBSVvKRrsbOA1WjnEgvYbj+bxpXMyuc=";
+    rev = "6be531685f308f081675034904d89c9e920be996";
+    sha256 = "sha256-j/LjX41ajTzUTrwsvLO8fjPCJXwHkSe8P9ClYOj51I4=";
   };
 
   buildInputs = [ olm ];
 
-  vendorSha256 = "sha256-WJJHAppvoeG4t7cY6fbuT8HKjYwEmg9PSAqyMcsiKh0=";
+  vendorSha256 = "sha256-apQSgwzKmPa5B7CbDhSzr7xgusLNCXJ8ww/3IWUZTjU=";
 
   doCheck = false;
 
-  runVend = true;
+  proxyVendor = true;
 
   meta = with lib; {
     mainProgram = "discord";
-    homepage = "https://gitlab.com/beeper/discord";
+    homepage = "https://github.com/mautrix/discord";
     description = "Matrix <-> Discord hybrid puppeting/relaybot bridge";
     license = licenses.agpl3;
     maintainers = with maintainers; [ pacman99 ];
